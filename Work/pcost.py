0e6168f987429e99ef6fdd22e39e336427b9eb45
Mohit <111426594+mohitxya@users.noreply.github.com>
2024-12-20 23:59:45 +0530

    update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
-# pcost.py
-#
-# Exercise 1.27
+sum=0
+with open("Data/portfolio.csv") as f:
+    next(f)
+    for line in f:
+        row=line.split(",")
+        a=int(row[1])
+        row2=row[2].split("\n")
+        b=float(row2[0])
+        sum+=a*b
+        #sum+=int(row[1])*int(row[2])
+print(f"The total cost is: {sum}")
