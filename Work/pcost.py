1e32e2d1e429a9fe693570ac5e1d1d889b2702f9
Mohit <111426594+mohitxya@users.noreply.github.com>
2025-01-25 23:47:08 +0530

    change to report.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,22 @@
+import csv
+import sys
 
 def portfolio_cost(file):
     sum=0
-    with open(file) as f:
-        next(f)
-        for line in f:
-            row=line.split(",")
-            a=int(row[1])
-            row2=row[2].split("\n")
-            b=float(row2[0])
-            sum+=a*b
-            #sum+=int(row[1])*int(row[2])
-    print(f"The total cost is: {sum}")
+    try:
+        with open(file) as f:
+            rows=csv.reader(f)
+            headers=next(rows)
+            for line in rows:
+                sum+=float(line[1])*float(line[2])
+    except FileNotFoundError:
+        print("Please enter a legit file name")
+    return sum
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
